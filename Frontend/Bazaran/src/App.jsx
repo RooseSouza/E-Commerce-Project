6e08d5c3a285bac44c61adb3be53970d106c7c13
RooseSouza <123053090+RooseSouza@users.noreply.github.com>
2026-01-28 11:45:25 +0530

    added vendor dashboard route

--- Frontend/Bazaran/src/App.jsx
+++ Frontend/Bazaran/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
                <Route path="/product" element={<ProductDetails />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/profile" element={<UserProfile />} />
+              <Route path="/vendor-dashboard" element={<VendorDashboard />} />
             </Routes>
           </Router>
         </div>
