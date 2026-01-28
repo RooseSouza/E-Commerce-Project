e2bc93b9e9b313f56ed0d4bcbbe33c302ec0fafa
RooseSouza <roose1209souza@gmail.com>
2026-01-28 14:44:17 +0530

    changes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
+const { getMyProfile } = require("../controllers/userController");
 
 const {
   registerUser,
@@ -16,5 +17,6 @@ router.post("/google", googleLogin); // Google login route
 
 // Protected route
 router.get("/me", protect, getMe);
+router.get("/me/profile", protect, getMyProfile);
 
 module.exports = router;
