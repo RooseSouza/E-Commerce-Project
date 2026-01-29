8dfbc53efaedc1f8dab861d59b3e70cc6c7a635d
RooseSouza <roose1209souza@gmail.com>
2026-01-29 14:41:23 +0530

    add new address functionality

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -26,3 +26,12 @@ exports.protect = async (req, res, next) => {
     res.status(401).json({ message: "Invalid token" });
   }
 };
+// Role-based access
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied for this role" });
+    }
+    next();
+  };
+};
