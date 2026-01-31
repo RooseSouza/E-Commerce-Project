9b64e2626739b808dcb0d3947d6f29a350951b0a
Roosevelt Desouza <roose1209souza@gmail.com>
2026-01-31 11:58:01 +0530

    cloudinary fix

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary");
 
 // configure the V2 instance (this is what multer-storage-cloudinary expects)
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // use exact name from env
+  cloud_name: process.env.CLOUDINARY_NAME,  // use exact name from env
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
