c0fac868fce54d35e071540e136191587e8435a5
RooseSouza <123053090+RooseSouza@users.noreply.github.com>
2026-01-31 13:10:52 +0530

    Update cartRoutes.js

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getCart, addToCart, updateCartItem, removeFromCart } = require('../controllers/cartController');
+const { getCart, addToCart, updateCartItem, removeFromCart, getCartSummary } = require('../controllers/cartController');
 
 router.use(protect); // All cart routes require login
 
@@ -9,5 +9,6 @@ router.get('/', getCart);
 router.post('/add', addToCart);
 router.put('/update', updateCartItem);
 router.delete('/remove/:productId', removeFromCart);
+router.get("/summary", protect, getCartSummary);
 
 module.exports = router;
