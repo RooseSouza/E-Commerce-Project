cf6841d2733103093302b884a82625008f33bbd7
Roosevelt Desouza <roose1209souza@gmail.com>
2026-01-31 11:46:32 +0530

    cloudinary fix

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,15 +1,16 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
+const multerStorageCloudinary = require("multer-storage-cloudinary");
+const CloudinaryStorage = multerStorageCloudinary.CloudinaryStorage || multerStorageCloudinary;
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary, // pass the full module
+  cloudinary: cloudinary,
   params: {
-    folder: "bazaran_products",
+    folder: "bazaran_products", // The folder name in your Cloudinary dashboard
     allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
