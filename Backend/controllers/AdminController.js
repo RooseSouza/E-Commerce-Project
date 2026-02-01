30849d1b74e6b5b540d43d1504def76c43c7e55f
RooseSouza <123053090+RooseSouza@users.noreply.github.com>
2026-02-01 16:51:10 +0530

    Update AdminController.js

--- Backend/controllers/AdminController.js
+++ Backend/controllers/AdminController.js
@@ -293,19 +293,4 @@ exports.getUserOrders = async (req, res) => {
 
     res.json(orders);
 
-/**
- * 🗑️ Admin Delete Product
- */
-exports.deleteProductAdmin = async (req, res) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    res.json({ message: "Product deleted successfully" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
 };
